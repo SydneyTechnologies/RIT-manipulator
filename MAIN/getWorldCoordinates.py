b644da9c854cba95b799d52d701aa080ce430737
sydneyIdundun <sydney.idundun@gmail.com>
2023-03-01 18:39:52 +0400

    edited the constraints file and pushed the code

--- MAIN/getWorldCoordinates.py
+++ MAIN/getWorldCoordinates.py
@@ -1,15 +1,19 @@
 def getRealWorld(x_pixel, y_pixel):
+    # TESTING EQUATION
     #x = 0.4471x - 423.25
     #y = 5.4878 + 355
 
-    XGRADIENT = 0.445
-    XINTERCEPT = -419.2
+    # RIT CALIBRATION EQUATION
+
+
+    XGRADIENT = 0.447
+    XINTERCEPT = -417.64
 
 
 
 
-    YGRADIENT = -0.4487
-    YINTERCEPT = 843.55
+    YGRADIENT = -0.452
+    YINTERCEPT = 842.05
 
     # x_pixel = int(input("please enter the x coordinates in pixels"))
     # y_pixel = int(input("please enter the y coordinates in pixels"))
